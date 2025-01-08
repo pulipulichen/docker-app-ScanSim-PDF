af5ef89032fee7f6cfb5e613318f7ada446f9785
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-01-08 11:28:27 +0800

    西元2025年01月08日 (週三) 11時28分27秒 CST

--- app/processSinglePDF.js
+++ app/processSinglePDF.js
@@ -74,7 +74,10 @@ async function rotateImages (imageDir) {
     let command = `convert "${filePath}" -rotate "${angle}" "${filePath}"`
     await ShellExec(command)
 
-    command = `convert "${filePath}" ( -evaluate AddNoise Gaussian ) ( -size 100x1 gradient:gray50-gray80 -resize x2000! ) -compose Multiply -composite "${filePath}"`
+    command = `convert "${filePath}" -evaluate AddNoise Gaussian "${filePath}"`
+    await ShellExec(command)
+
+    command = `convert "${filePath}" \\( -size 100x1 gradient:gray50-gray80 -resize x2000! \\) -compose Multiply -composite "${filePath}"`
     await ShellExec(command)
   }
 }
